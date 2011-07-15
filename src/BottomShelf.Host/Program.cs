ed25cfc77c0faafcbafe6008c9a383ebaae323e4
Tyrone Groves <grovest@digitalev.com>
2011-07-15 16:43:30 -0500

    added test project

--- src/BottomShelf.Host/Program.cs
+++ src/BottomShelf.Host/Program.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BottomShelf.Host
+﻿namespace BottomShelf.Host
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
